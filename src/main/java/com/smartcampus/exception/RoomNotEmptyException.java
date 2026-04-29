9de206866053335a5a7d35ade2d77f7ad43a0ae4
AryanPaudel8 <rnrn97967@gmail.com>
2026-04-29 22:03:05 +0545

    feat: add exception handling mappers and complete SensorResource with validation and sub-resource locator

--- src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyException.java
@@ -3,11 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.smartcampus.exception;
-
+import java.util.List;
 /**
  *
  * @author aryanpaudel
  */
-public class RoomNotEmptyException {
-    
+public class RoomNotEmptyException extends RuntimeException {
+ 
+    private final String roomId;
+    private final List<String> sensorIds;
+ 
+    public RoomNotEmptyException(String roomId, List<String> sensorIds) {
+        super("Cannot delete room '" + roomId + "': it still has "
+                + sensorIds.size() + " sensor(s) assigned.");
+        this.roomId = roomId;
+        this.sensorIds = sensorIds;
+    }
+ 
+    public String getRoomId() {
+        return roomId;
+    }
+ 
+    public List<String> getSensorIds() {
+        return sensorIds;
+    }
 }
